a3ff0df6255a434b7766862ea48ffe56ebe3fed5
caligian <caligian@protonmail.com>
2023-11-19 21:08:40 +0530

    more changes

--- src/optparser.lua
+++ src/optparser.lua
@@ -1,5 +1,13 @@
 require("lua-utils")
 
+local function appendlist(x, elems)
+  local n = #x
+
+  for i = 1, #elems do
+    x[n + i] = elems[i]
+  end
+end
+
 local function find(tbl, elem, offset)
   for i = offset, #tbl do
     if tbl[i] == elem then
@@ -33,22 +41,25 @@ local function default_switch(short, long, overrides)
     args = false,
     required = false,
     multiple = true,
-    nargs = "+",
+    nargs = "*",
     deps = false,
     undeps = false,
     post = false,
     assert = false,
     help = false,
     name = false,
+    metavar = false,
   }
 
   for key, value in pairs(overrides or {}) do
     result[key] = value
   end
 
-  result.name = long and long or short
   result[1] = short
   result[2] = long
+  result.name = long and long or short
+
+  local metavar = result.metavar or result.name:upper()
 
   return result
 end
@@ -67,7 +78,7 @@ local function find_switch(haystack, switch)
     errfmt("multiple switch instances not allowed (required nargs: %s)", switch, nargs)
   end
 
-  collected = collected or {}
+  local collected = {}
   if long_pos then
     for i = 1, #long_pos do
       collected[#collected + 1] = default_switch(switch[1], switch[2], switch)
@@ -95,9 +106,7 @@ local function find_switches(haystack, switches)
 
   for i = 1, #switches do
     local ok = find_switch(haystack, switches[i])
-    for j = 1, #ok do
-      results[#results + 1] = ok[j]
-    end
+    appendlist(results, ok)
   end
 
   table.sort(results, function(x, y)
@@ -110,7 +119,7 @@ end
 local function slicelist(x, from, till)
   local out = {}
 
-  for i = from + 1, till - 1 do
+  for i = from, till - 1 do
     out[#out + 1] = x[i]
   end
 
@@ -123,13 +132,13 @@ local function extract_args(haystack, switches, len, i)
   i = i or 1
 
   if len == i then
-    switches[i].args = slicelist(haystack, switches[i].pos, #haystack + 1)
+    switches[i].args = slicelist(haystack, switches[i].pos + 1, #haystack + 1)
     return switches
   end
 
   local first = switches[i]
   local second = switches[i + 1]
-  local args = slicelist(haystack, first.pos, second.pos)
+  local args = slicelist(haystack, first.pos + 1, second.pos)
   first.args = args
 
   return extract_args(haystack, switches, len, i + 1)
@@ -143,61 +152,159 @@ local function collapse(switches)
     local x = switches[i]
     local use = x[2] or x.name
     local new
+    local nargs = x.nargs
 
     if not res[use] then
       res[use] = default_switch(x[1], x[2], x)
       new = res[use]
       new.args = {}
-      new.pos = nil
     else
       new = res[use]
     end
 
-    for j = 1, #x.args do
-      new.args[#new.args + 1] = x.args[j]
+    local args = new.args
+    local passed = #args
+    local is_one_or_zero = nargs == "?"
+    local is_num = type(nargs) == "number"
+    local wrong_nargs = (is_one_or_zero and passed > 1) or (is_num and passed > nargs)
+
+    if wrong_nargs then
+      errfmt("required nargs: %s, got %d", new, nargs, passed)
     end
+
+    appendlist(args, x.args)
 
     if len == i then
       res.__last = new
+    elseif i == 1 then
+      res.__first = new
     end
   end
 
   return res
 end
 
-local function extract_positional(haystack, switches)
+local function get_positional_and_keyword(haystack, switches)
+  local fulllen = #haystack
   local last = switches.__last
+  local first = switches.__first
+  local last_pos = last.pos
+  local first_pos = first.pos
+  local first_args = first.args
+  local before_first = first_pos > 1 and slicelist(haystack, 1, first_pos)
+  local last_args = last.args
+  local last_nargs = last.nargs
+  local last_passed_nargs = #last_args
+  local sep = find(last_args, "--", last_pos)
+  local after_sep = sep and slicelist(last_args, sep + 1, last_passed_nargs)
   local positional = {}
-
-  ---
-  -- pass
-  --
   switches.__last = nil
+  switches.__first = nil
+
+  if before_first then
+    appendlist(positional, before_first)
+  end
+
+  --- last switch
+  local is_num = type(last_nargs) == "number"
+  local ok = (last_nargs == "+" and last_passed_nargs > 0)
+    or (is_num and last_nargs == last_passed_nargs)
+    or true
+  local function fail()
+    errfmt("required nargs: %s, got %d", last, last_nargs, last_passed_nargs)
+  end
+
+  if not ok then
+    if is_num then
+      if last_nargs - last_passed_nargs < 0 then
+        fail()
+      else
+        last_args = slicelist(last_args, 1, last_nargs + 1)
+        appendlist(positional, slicelist(last_args, last_nargs + 1, fulllen + 1))
+      end
+    else
+      fail()
+    end
+  end
+
+  if after_sep then
+    appendlist(positional, after_sep)
+  end
+
+  switches[last.name] = last
+  switches[first.name] = first
+
   return positional, switches
 end
 
-local function verify(switches)
-  --- check nargs and run assert
+local function parse_args(haystack, positional_spec, keyword_spec)
+  local positional, keyword = get_positional_and_keyword(haystack, keyword_spec)
+  local results = {}
+
+  -- post-process positional, keyword
+
+  return results
 end
 
-local function fullhelp(switches)
+local function help(positional_spec, keyword_spec)
+  --- gen help
+  return text
 end
 
-local function optparser()
-  local parser = {switches = {}}
+local function optparser(haystack)
+  haystack = haystack or arg
+  local parser = { switches = { positional = {}, keyword = {} } }
+  local pos, kv = parser.switches.positional, parser.switches.keyword
 
-  function parser:add(short, long, specs)
+  local function add(switch, positional)
+    if positional then
+      pos[#pos+1] = switch
+    else
+      kv[#kv+1] = switch
+    end
+  end
+
+  function parser:append(name, specs)
+    add(default_switch(name, nil, specs), true)
+  end
+
+  function parser:on(short, long, specs) 
+    add(default_switch(short, long, specs))
+  end
+
+  function parser:parse()
+    return parse_args(haystack, pos, kv)
+  end
+
+  function parser:help()
+    return help(pos, kv)
+  end
+
+  function parser:print()
+    print(help(pos, kv))
+  end
+
+  if find(haystack, '--help') then
+    parser:print()
+    os.exit(0, true)
   end
 
   return parser
 end
 
-local haystack = split("--switch 1 2 3 -x 2 -x 3 -x 4 -x 6", " ")
-local x = default_switch("x", "switch")
-local parsed = find_switches(haystack, { x })
-extract_args(haystack, parsed)
-pp(collapse(parsed))
+local function test()
+  local haystack = split("1 2 3 4 --x_switch 1 2 3 -x 2 -x 3 -x 4 -x 6 -y 1 2 3 4 -y -1", " ")
+  local x = default_switch("x", "x_switch")
+  local y = default_switch("y", "y_switch")
+  local parsed = find_switches(haystack, { x, y })
 
+  extract_args(haystack, parsed)
 
+  parsed = collapse(parsed)
+  local pos, kv
+  pos, kv = get_positional_and_keyword(haystack, parsed)
+  pp({ pos, kv })
+end
 
+test()
 return optparser
